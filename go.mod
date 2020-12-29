e1b5c266f1991bea023095a3c4137044ef28a96c
Chonlasith Jucksriporn <chonlasith@gmail.com>
2020-12-29 21:49:41 +0700

    Add datasets updater

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/chonla/dataman
+
+go 1.15
+
+require (
+	github.com/dustin/go-humanize v1.0.0
+	github.com/mitchellh/go-homedir v1.1.0
+	gopkg.in/yaml.v2 v2.4.0
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/chonla/dataman/generator"
+	"github.com/chonla/dataman/updater"
 )
 
 var AppName = "dataman"
@@ -17,6 +18,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	if args[0] == "update" {
+		e := updateDatasets()
+		if e != nil {
+			fmt.Println(e)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
 	if args[0] == "version" {
 		printVersion()
 		os.Exit(0)
@@ -27,20 +37,39 @@ func main() {
 		os.Exit(0)
 	}
 
-	g, e := generator.New(args[0])
+	if args[0] == "gen" {
+		g, e := generator.New(args[0])
 
-	if e != nil {
-		fmt.Println(e)
-		os.Exit(1)
+		if e != nil {
+			fmt.Println(e)
+			os.Exit(1)
+		}
+
+		g.Generate()
+		os.Exit(0)
 	}
 
-	g.Generate()
+	printUsage()
+	os.Exit(1)
 }
 
 func printUsage() {
-	fmt.Println("dataman <configuration-file.yml>")
+	fmt.Printf("%s - a random data generator\n", AppName)
+	fmt.Println("")
+	fmt.Printf("%s gen <file.yml>    generate random date from given <file.yml> config\n", AppName)
+	fmt.Printf("%s update            download latest datasets\n", AppName)
+	fmt.Printf("%s version           show %s version\n", AppName, AppName)
+	fmt.Printf("%s help              show help\n", AppName)
+	fmt.Println("")
+	fmt.Println("project page: https://github.com/chonla/dataman")
 }
 
 func printVersion() {
 	fmt.Printf("%s v.%s", AppName, AppVersion)
+}
+
+func updateDatasets() error {
+	fmt.Println("updating datasets...")
+	err := updater.Update()
+	return err
 }
